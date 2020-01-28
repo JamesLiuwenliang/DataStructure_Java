bc51bd588ce2a9fc48c9754fda55d647b2c0e553
= <=>
2020-01-28 21:46:47 +0900

    first commit

---
+++ Chap01/FuckJava.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class FuckJava {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		//输入并输出一个整数
+		int n = in.nextInt();
+		System.out.println(n);
+		//输入并输出一个字符串
+		String s = in.next();
+		System.out.println(s);
+		//输入并输出一个字符串
+		String ss = in.nextLine();
+		System.out.println(ss);
+		
+	}
+ 
+}
+
