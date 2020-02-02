9dd9e23fdb65dba90c1634927c0e509e1177b11b
= <=>
2020-02-02 14:52:47 +0900

    add main.java

--- Chap02/ArrayList01.java
+++ Chap02/ArrayList01.java
@@ -1,7 +1,5 @@
-public class ArrayList01 {
-    public static void main(String[] args){
-        System.out.println("Hello World...");
-        System.out.println("Hello World...");
-        System.out.println("Hello World...");
-    }
-}
+public class ArrayList01{
+
+
+
+} 
---
+++ Chap02/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+
+        int[] scores = new int[]{100,99,59};
+        for (int score : scores){
+            System.out.println(score);
+        }
+        System.out.println("Just Test");
+
+        //ArrayList01 array = new ArrayList01();
+
+
+        System.out.println("Hello World...");
+
+    }
+}
