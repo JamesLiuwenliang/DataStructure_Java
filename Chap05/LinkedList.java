93c2f9af074ee4468849b66cf7ae8c6ba4adec69
JamesLiuwenliang <13114817178@163.com>
2020-02-09 00:55:03 +0900

    Linked is still creating. (2019-2-9)

--- Chap05/LinkedList.java
+++ Chap05/LinkedList.java
@@ -1,10 +1,16 @@
+/**
+ * dummyHead : 虚拟头节点,不占size
+ * 
+ * @param <E>
+ */
 public class LinkedList<E> {
 
-    private Node head;
+    private Node dummyHead;
     private int size ;
 
     public LinkedList(){
-        head = null;
+        Node node = new Node (null,null);
+        dummyHead = node;
         size = 0;
     }
 
@@ -16,66 +22,90 @@ public class LinkedList<E> {
         return size == 0;
     }
 
-    public void addFirst (E e){
-        Node node = new Node<>(e);
-        node.next = head;
-        head = node;
-        //head = new Node(e,head);
-
-        size++;
-
+    public void addFirst(E e){
+        insert(e,0);
     }
 
+    public void addLast(E e){
+        insert(e,size);
+    }
+
+    //pre 是插入节点的前一个节点
     public void insert(E e ,int pos){
-        if(pos == 0){
-            addFirst(e);
-            return ;
-        }
-        Node node = new Node<>(e);
-        Node current = head;
-        for(int i=0 ;i<pos ;i++){
-            current = current.next;
+
+        if(pos < 0 || pos >size){
+            throw new IllegalArgumentException("Add failed. Illegal position.");
         }
 
-        node.next = current.next;
-        current = node;
+        Node pre = dummyHead;
+        for(int i=0 ;i<pos ;i++){
+            pre = pre.next;
+        }
+
+        Node node = new Node(e);
+        node.next = pre.next;
+        pre.next = node;
+        //pre.next = new Node(e, current.next);
+
         size ++;
 
     }
-//
-//    public Node delete (int pos){
-//        Node delNode = null;
-//        Node current = head;
-//        for(int i=0 ;i<pos ;i++){
-//            current = current.next;
-//        }
-//        delNode = current.next;
-//        current = current.next.next;
-//        size--;
-//        return delNode;
-//
-//    }
-//
-//    public void change(Node e ,int pos){
-//        Node current = head;
-//        for (int i = 0; i < pos; i++) {
-//            current = current.next;
-//        }
-//        current.next = e;
-//        e.next = current.next;
-//
-//    }
-//
-//    public int find(Node e ){
-//        int pos = -1 ;
-//        Node current = head;
-//        for (int i = 0; i<size ; i++) {
-//
-//            if(current == e){
-//                pos = i;
-//            }
-//            current = current.next;
-//        }
-//        return pos;
-//    }
+
+
+    public Node delete (int pos){
+
+        Node delNode = null;
+        Node pre = dummyHead;
+        for(int i=0 ;i<pos ;i++){
+            pre = pre.next;
+        }
+        delNode = pre.next;
+        pre = pre.next.next;
+        size--;
+        return delNode;
+
+    }
+
+    public void change(E e ,int pos){
+        Node current = dummyHead.next;
+        for (int i = 0; i < pos; i++) {
+            current = current.next;
+        }
+        current.e = e;
+    }
+
+    public boolean find(E e ){
+
+        Node current = dummyHead.next;;
+        for (int i = 0; i<size ; i++) {
+
+            if(current.e.equals(e)){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public E get(int pos){
+        if(pos < 0 || pos >size){
+            throw new IllegalArgumentException("Add failed. Illegal position.");
+        }
+        Node current = dummyHead.next;
+        for(int i=0 ; i<pos ;i++){
+            current = current.next;
+        }
+        return (E) current.e;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        Node current = dummyHead.next;
+        while(cur. != null){
+
+        }
+
+    }
+
 }
